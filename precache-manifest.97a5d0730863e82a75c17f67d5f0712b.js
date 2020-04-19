8934a86c2630d8b65e8dbeaa1e90195de38a0a08
pch2013 <pch2103@gmail.com>
2020-04-19 22:55:05 +0300

    Updates

---
+++ precache-manifest.97a5d0730863e82a75c17f67d5f0712b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5265edb813ab417757a0d01e88dd38c8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b49262247d2721fa6879",
+    "url": "/todo/static/css/2.658eb2da.chunk.css"
+  },
+  {
+    "revision": "632a416a373364cd9daf",
+    "url": "/todo/static/css/main.aee7b98a.chunk.css"
+  },
+  {
+    "revision": "b49262247d2721fa6879",
+    "url": "/todo/static/js/2.843a9020.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/todo/static/js/2.843a9020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632a416a373364cd9daf",
+    "url": "/todo/static/js/main.270bcd28.chunk.js"
+  },
+  {
+    "revision": "f6fc1f9f0f989739bc99",
+    "url": "/todo/static/js/runtime-main.1e4610cc.js"
+  }
+]);
